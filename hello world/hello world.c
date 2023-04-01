47b32a09b7156234d363c00084b69909d760addf
zym-null <125949631+zym-null@users.noreply.github.com>
2023-04-01 15:44:53 +0800

    Add files via upload

---
+++ hello world/hello world.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void)
+{
+	//第一个C程序，include是预处理指令，引入头文件
+	/*一个C程序一定有且仅有一个main（）函数，程序从main函数开始运行
+	* printf声明函数用于格式化输出在屏幕上
+	* return 0表示回到整形0
+	* 每一个语句后都要用;
+	* stdio.h,表示头文件
+	*/
+	printf("hello wrold!\n");
+	return 0;
+}
