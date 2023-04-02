0b8d1df0052276949d09cd90d39ae57ba50ab42f
zym-null <125949631+zym-null@users.noreply.github.com>
2023-04-02 20:43:49 +0800

    Add files via upload

---
+++ 对象和类型的字节大小/源.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+/*
+sizeof()用于获取对象和类型的字节大小*/
+{
+	printf("int : %lu \n",sizeof (int));
+	printf("char : %d \n",sizeof(char));
+	printf("short : %d \n", sizeof(short));
+	printf("long : %d \n",sizeof(long));
+	printf("unsigned int : %d \n",sizeof(unsigned int));
+	getch();
+	//getch（）用于使程序运行窗口停留
+	return 0;
+}
